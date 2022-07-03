20bf0795eb3b7ea682e0ffd384471b6b9e06d382
robin.xiang <liverpool8056@163.com>
2022-07-03 12:08:01 +0800

    add hello module

---
+++ hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int hello(*char [] name){
+    printf("Hello %s", name);
+}
--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <hello.h>
 
 int main(){
+    int a = 1;
     printf("Hello");
+    printf("a: %d", a);
 }
